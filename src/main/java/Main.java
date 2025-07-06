fc436e2afb5d0fd912aa2baaaf647e51d1024a05
Ali Biner <alibiner.github@gmail.com>
2025-07-06 13:19:10 +0300

    create project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    private String password = "123456";
+    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.start();
+    }
+
+    public void start(){
+        System.out.print("Şifrenizi giriniz: ");
+        String currentPassword = scanner.next();
+
+        if (currentPassword.equals(password))
+            System.out.println("Giriş yapıldı.");
+        else if (!currentPassword.equals(password)) {
+            System.out.print("Şifrenizi sıfırlamak istiyorsanız 1'e basınız: ");
+            int select = scanner.nextInt();
+            switch (select){
+                case 1:
+                    changePassword();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void changePassword(){
+        System.out.print("Yeni şifrenizi giriniz: ");
+        String newPassword = scanner.next();
+        while (newPassword.equals(password)){
+            System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+            newPassword = scanner.next();
+        }
+        
+        System.out.println("Şifre oluşturuldu");
     }
 }
